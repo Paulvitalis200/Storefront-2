cd3348f61b470624763876e40dd60c50139e22c9
Paul Otieno <paul.otieno@gitlab.com>
2025-11-17 21:37:38 +0300

    Add files

---
+++ api/filters.py
@@ -0,0 +1,20 @@
+import django_filters
+from rest_framework import filters
+
+from api.models import Product
+
+class InStockFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(stock__gt=0)
+
+
+class ProductFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='iexact')
+
+    class Meta:
+        model = Product
+        fields = {
+            'name': ['exact', 'contains'], 
+            'price': ['exact', 'lt', 'gt', 'range']
+        }
+        
