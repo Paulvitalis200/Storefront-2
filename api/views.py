919a781cecc7ad9a7855e67cb566c71e734cf2d2
Paul Otieno <paul.otieno@gitlab.com>
2025-11-18 11:23:33 +0300

    Add Throttling

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Max
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie, vary_on_headers
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics, viewsets
 from rest_framework.decorators import action
@@ -12,11 +13,16 @@ from rest_framework.views import APIView
 
 from api.filters import InStockFilterBackend, OrderFilter, ProductFilter
 from api.models import Order, Product, User
-from api.serializers import (OrderCreateSerializer, OrderSerializer, ProductInfoSerializer,
-                             ProductSerializer, UserSerializer)
+from api.serializers import (OrderCreateSerializer, OrderSerializer,
+                             ProductInfoSerializer, ProductSerializer,
+                             UserSerializer)
+
+from rest_framework.throttling import ScopedRateThrottle
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
+    throttle_scope = 'products'
+    throttle_classes = [ScopedRateThrottle]
     queryset = Product.objects.order_by('pk')
     serializer_class = ProductSerializer
     filterset_class = ProductFilter
@@ -64,6 +70,7 @@ class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class OrderViewSet(viewsets.ModelViewSet):
+    throttle_scope = 'orders'
     queryset = Order.objects.prefetch_related(
         'items__product'
     )
@@ -72,6 +79,11 @@ class OrderViewSet(viewsets.ModelViewSet):
     pagination_class = None
     filterset_class = OrderFilter
     filter_backends = [DjangoFilterBackend]
+
+    @method_decorator(cache_page(60 * 15, key_prefix='order_list'))
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
