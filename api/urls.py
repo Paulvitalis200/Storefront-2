4e7dcf68f27c772d16d9e087587fce266a1d5933
Paul Otieno <paul.otieno@gitlab.com>
2025-11-18 15:59:54 +0300

    Add Tests

--- api/urls.py
+++ api/urls.py
@@ -4,14 +4,13 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('products/', views.ProductListCreateAPIView.as_view()),
-    # path('products/create/', views.ProductCreateAPIView.as_view()),
     path('products/info/', views.ProductInfoAPIView.as_view()),
-    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
     path('users/', views.UserListView.as_view()),
     # path('orders/', views.OrderListAPIView.as_view()),
     # path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
 ]
 
 router = DefaultRouter()
-router.register('orders', views.OrderViewSet)
+router.register('orders', views.OrderViewSet,)
 urlpatterns += router.urls
